ea127c99b6d72ff953486e072ff6d5baa7cf79e8
Nilesh Bhoi <Nilesh Bhoi@192.168.1.206>
2023-12-17 20:51:51 +0530

    adding few new codes

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -43,13 +44,16 @@ public class BaseClass {
 	   
 	   Reporter.log("Setup Done- Test Can be Started", true);
     }
-   
+	
+    @Parameters({"browser","urlToBeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String browser,String url)
 	{
 		Reporter.log("Trying to start the browser and application ready ", true);
 		
-		driver=BrowserFactory.startApplication(driver,config.getBrowser(),config.getStagingURL());
+		//driver=BrowserFactory.startApplication(driver,config.getBrowser(),config.getStagingURL());
+		
+		driver=BrowserFactory.startApplication(driver, browser, url);
 		
 		Reporter.log("Browser and Application are startup and running", true);
 	}
